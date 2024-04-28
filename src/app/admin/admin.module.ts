6d5ecf67b62ef61bce89b7b7aa3ec18181222533
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-04-27 23:32:58 -0500

    feat(admin): add primary button atom

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PrimaryButtonComponent } from './atom/primary-button/primary-button.component';
+
+
+
+@NgModule({
+  declarations: [
+    PrimaryButtonComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
