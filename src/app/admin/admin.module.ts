0e677e6eda6083397a16518adf0271db62de2a34
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-05-01 14:04:21 -0500

    chore: move shared components to app folder

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,17 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PrimaryButtonComponent } from './components/atoms/primary-button/primary-button.component';
-import { BasicInputComponent } from './components/molecules/basic-input/basic-input.component';
-import { CloseButtonComponent } from './components/atoms/close-button/close-button.component';
-import { ModalComponent } from './components/organisms/modal/modal.component';
 
 
 @NgModule({
   declarations: [
-    PrimaryButtonComponent,
-    BasicInputComponent,
-    CloseButtonComponent,
-    ModalComponent,
   ],
   imports: [
     CommonModule
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimaryButtonComponent } from './components/atoms/primary-button/primary-button.component';
+import { BasicInputComponent } from './components/molecules/basic-input/basic-input.component';
+import { CloseButtonComponent } from './components/atoms/close-button/close-button.component';
+import { ModalComponent } from './components/organisms/modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimaryButtonComponent,
+    BasicInputComponent,
+    CloseButtonComponent,
+    ModalComponent,
   ],
   imports: [
     BrowserModule,
